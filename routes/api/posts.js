7881574bd1346d522199e2fe2d1a6e1e9df54862
Mimi <trish21215@yahoo.com>
2023-05-06 18:05:46 -0400

    finish routes/api to include multer

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const postsCtrl = require('../../controllers/posts');
+const multer = require('multer');
+const upload = multer();
 
-
-router.get('/', postsCtrl.index)
+router.post('/', upload.single('photo'), postsCtrl.create);
+router.get('/', postsCtrl.index);
 
 module.exports = router;
